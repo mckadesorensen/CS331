42a43eaa89ffce8c7fe9cb42a299669a5a865c1c
McKade Sorensen <49543713+mckadesorensen@users.noreply.github.com>
2020-02-10 11:05:44 -0900

    Add files via upload

---
+++ pa2.lua
@@ -0,0 +1,40 @@
+-- McKade Sorensen
+-- Last revision date: 02/09/2019
+-- Source for module pa2
+
+local pa2 = {} -- Table that contains module members
+
+
+-- Returns a table of all truthy values in t
+function pa2.filterTable(p, t)
+  local truthy_keypairs = {} -- Table that stores truthy values
+
+  for key, value in pairs(t) do
+    if p(value) == true and p(value) ~= nil then
+      truthy_keypairs[key] = value
+    end
+  end
+
+  return truthy_keypairs
+end
+
+
+-- Returns a string with str repeated int many of times
+function pa2.concatMax(str, int)
+  return string.rep(str, math.floor(int/(#str)))
+end
+
+
+-- Write what it does
+function pa2.collatz()
+  -- Figue the what f**K you need to do.
+end
+
+
+-- Write what it does
+function pa2.allSubs()
+  -- Figue the what f**K you need to do.
+end
+
+
+return pa2
